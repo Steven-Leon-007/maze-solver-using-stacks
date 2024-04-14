0e1581db4a3e200cde73fbdff0b5f4af5a05c08d
Steven-Leon-007 <estivlinyor@gmail.com>
2024-04-14 15:33:10 -0500

    Added logic for solve mazes through LIFO interation

--- src/main/java/com/estivman/stacks_project/maze_solver/controller/MazeSolverController.java
+++ src/main/java/com/estivman/stacks_project/maze_solver/controller/MazeSolverController.java
@@ -1,6 +1,7 @@
 package com.estivman.stacks_project.maze_solver.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,15 +23,24 @@ public class MazeSolverController {
     @Value("${maze.example.location}")
     private String mazeExample;
 
+    // TODO: Change for post method with file manager
     @GetMapping()
-    public ResponseEntity<Object> loadMaze() throws IOException{
+    public ResponseEntity<Object> loadMaze() throws IOException {
         try {
             MazeSection[][] maze = mazeSolverService.loadMaze(mazeExample);
-            
+
             return ResponseEntity.status(HttpStatus.OK).body(maze);
         } catch (IOException e) {
             throw e;
         }
+
+    }
+
+    @GetMapping("/solve")
+    public ResponseEntity<Object> solveMaze() throws IOException {
+        this.loadMaze();
+        List<MazeSection> result = mazeSolverService.solveMaze();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
 
     }
 }
--- src/main/java/com/estivman/stacks_project/maze_solver/service/MazeSolverService.java
+++ src/main/java/com/estivman/stacks_project/maze_solver/service/MazeSolverService.java
@@ -1,6 +1,7 @@
 package com.estivman.stacks_project.maze_solver.service;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
@@ -14,7 +15,7 @@ import com.estivman.stacks_project.maze_solver.utils.FileLoaderUtils;
 @Service
 public class MazeSolverService {
     private MazeSection[][] maze;
-    private Stack<MazeSection> stack;
+    private Stack<MazeSection> stackMaze;
     FileLoaderUtils fileLoaderUtils;
 
     @Autowired
@@ -38,8 +39,51 @@ public class MazeSolverService {
         return maze;
     }
 
-    public void solveMaze(){
-        
+    public Stack<MazeSection> solveMaze() {
+        stackMaze = new Stack<>();
+        stackMaze.push(this.findMazeStart());
+
+        while (!stackMaze.isEmpty()) {
+            MazeSection current = stackMaze.peek();
+            current.setSectionVisited(true);
+
+            if (current.getType() == SectionType.FINISH) {
+                break;
+            }
+
+            MazeSection[] sectionsAvailable = this.validSurroundings(current);
+
+            boolean canMove = false;
+            for (MazeSection section : sectionsAvailable) {
+                if (!section.isSectionVisited()) {
+                    stackMaze.push(section);
+                    canMove = true;
+                    break;
+                }
+            }
+            if (!canMove) {
+                stackMaze.pop();
+            }
+
+        }
+
+        for (MazeSection section : stackMaze) {
+            section.setType(SectionType.SOLUTION);
+        }
+
+        return stackMaze;
+    }
+
+    private MazeSection findMazeStart() {
+
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[0].length; j++) {
+                if (maze[i][j].getType() == SectionType.START) {
+                    return maze[i][j];
+                }
+            }
+        }
+        return null;
     }
 
     private MazeSection convertToSection(char character, int row, int col) {
@@ -59,5 +103,30 @@ public class MazeSolverService {
             default:
                 throw new IllegalArgumentException("Character unknown: " + character);
         }
+    }
+
+    private MazeSection[] validSurroundings(MazeSection section) {
+        int row = section.getRow();
+        int col = section.getColumn();
+        List<MazeSection> validNeighbors = new ArrayList<>();
+
+        int[] possibleMovesRows = { -1, 0, 0, 1 };
+        int[] possibleMovesCols = { 0, -1, 1, 0 };
+
+        for (int i = 0; i < possibleMovesCols.length; i++) {
+            
+            int newRow = row + possibleMovesRows[i];
+            int newCol = col + possibleMovesCols[i];
+
+            if (newRow >= 0 && newRow < maze.length &&
+                    newCol >= 0 && newCol < maze[0].length) {
+                MazeSection neighbor = maze[newRow][newCol];
+                if (neighbor.getType() != SectionType.WALL) {
+                    validNeighbors.add(neighbor);
+                }
+            }
+        }
+
+        return validNeighbors.toArray(new MazeSection[0]);
     }
 }
